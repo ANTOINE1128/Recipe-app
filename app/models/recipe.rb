148ef9cf155947423e0f49f51163ca9534d5efc6
ANTOINE1128 <makdessyantoine@gmail.com>
2023-09-13 12:16:51 +0300

    add recipies view

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
-  validates :name, presence: true
-  validates :preparation_time, presence: true
-  validates :cooking_time, presence: true
+  # validates :name, presence: true
+  # validates :preparation_time, presence: true
+  # validates :cooking_time, presence: true
   belongs_to :user
   has_many :recipefood
 end
