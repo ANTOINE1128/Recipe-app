3934eea80e04b52fafc33f86142c8ce1b8399436
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-09-13 13:03:55 +0100

    update files

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,56 +1,55 @@
 # app/controllers/recipes_controller.rb
 
 class RecipesController < ApplicationController
-    before_action :set_recipe, only: [:show, :edit, :update, :destroy]
-  
-    def index
-      @recipes = Recipe.all
+  before_action :set_recipe, only: %i[show edit update destroy]
+
+  def index
+    if current_user
+      @recipes = current_user.recipes.includes(:recipe_foods).order(created_at: :desc)
+    else
+      redirect_to new_user_session_path, alert: 'Please log in to view your foods.'
     end
-  
-    def new
-      @recipe = Recipe.new
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = current_user.recipes.new(recipe_params)
+
+    if @recipe.save
+      redirect_to recipes_path, Notice: 'Recipes added successfully'
+    else
+      flash[:notice] = @recipe.errors.full_messages.join(', ')
+      redirect_to request.referrer
     end
-  
-    def create
-        @recipe = Recipe.new(recipe_params)
-        if @recipe.save
-          respond_to do |format|
-            format.turbo_stream { render turbo_stream: turbo_stream.replace(@recipe) }
-            format.html { redirect_to @recipe, notice: 'Recipe was successfully created.' }
-          end
-        else
-          render :new
-        end
-      end
-      
-  
-    def show
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe, notice: 'Recipe was successfully updated.'
+    else
+      render :edit
     end
-  
-    def edit
-    end
-  
-    def update
-      if @recipe.update(recipe_params)
-        redirect_to @recipe, notice: 'Recipe was successfully updated.'
-      else
-        render :edit
-      end
-    end
-  
-    def destroy
-      @recipe.destroy
-      redirect_to recipes_url, notice: 'Recipe was successfully deleted.'
-    end
-  
-    private
-  
-    def set_recipe
-      @recipe = Recipe.find(params[:id])
-    end
-  
-    def recipe_params
-      params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id)
-    end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to recipes_url, notice: 'Recipe was successfully deleted.'
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end
-  
