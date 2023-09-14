a5f4d57c3dc9d284abaa5d0b12f1c0975a6bab27
ANTOINE1128 <makdessyantoine@gmail.com>
2023-09-14 16:01:09 +0300

    add test for recipe model

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -50,10 +50,10 @@ class RecipesController < ApplicationController
   end
 
   def destroy
-    @recipe.destroy
+    @recipe.recipe_foods.destroy_all
+    @recipe.destroy 
     redirect_to recipes_url, notice: 'Recipe was successfully deleted.'
   end
-
   private
 
   def set_recipe
---
+++ spec/models/recipe_spec.rb
@@ -0,0 +1,39 @@
+# spec/models/recipe_spec.rb
+
+require 'rails_helper'
+
+RSpec.describe Recipe, type: :model do
+  let(:user) { User.create(name: "Test User", email: "test@example.com", password: "password") }
+
+  
+  it "is valid with a name, preparation_time, cooking_time, and a user" do
+    recipe = Recipe.new(name: "Delicious Dish", preparation_time: 30, cooking_time: 60, user: user)
+    expect(recipe).to be_valid
+  end
+
+  it "is not valid without a name" do
+    recipe = Recipe.new(name: nil, preparation_time: 30, cooking_time: 60, user: user)
+    expect(recipe).to_not be_valid
+  end
+
+  it "is not valid without preparation_time" do
+    recipe = Recipe.new(name: "Delicious Dish", preparation_time: nil, cooking_time: 60, user: user)
+    expect(recipe).to_not be_valid
+  end
+
+  it "is not valid without cooking_time" do
+    recipe = Recipe.new(name: "Delicious Dish", preparation_time: 30, cooking_time: nil, user: user)
+    expect(recipe).to_not be_valid
+  end
+
+  it "is not valid without a user" do
+    recipe = Recipe.new(name: "Delicious Dish", preparation_time: 30, cooking_time: 60, user: nil)
+    expect(recipe).to_not be_valid
+  end
+
+  it "can have associated recipe foods" do
+    recipe = Recipe.new(name: "Delicious Dish", preparation_time: 30, cooking_time: 60, user: user)
+    recipe.recipe_foods.build(quantity: 2, food_id: 1) # You can replace 1 with a valid food_id
+    expect(recipe.recipe_foods).to_not be_empty
+  end
+end
